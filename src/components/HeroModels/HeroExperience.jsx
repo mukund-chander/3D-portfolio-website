ed4e585645389a0af6b03d1b4a83122a7b1a55f1
theunderdawg111 <mukundverse101@gmail.com>
2025-05-07 17:47:20 +0530

    Added Scroll Animation and Counter

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -20,7 +20,7 @@ const HeroExperience = () => {
         maxPolarAngle={Math.PI / 2}
       />
       <HeroLights />
-      <Particles />
+      <Particles count={100} />
       <group
         scale={isMobile ? 0.7 : 1}
         position={[0, -3.5, 0]}
