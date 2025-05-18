be4296f6393177050e707d1749baae72cac892a0
theunderdawg111 <mukundverse101@gmail.com>
2025-05-18 16:11:09 +0530

    Added Logo Animation display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
-
+import LogoSection from "./components/LogoSection";
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   );
 };
